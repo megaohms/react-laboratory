b29987c3ea6124161e14f77c96e5aab97e0ccac4
Megan Ring <megan.ring@linqia.com>
2017-06-25 11:21:03 -0700

    Upgrade webpack, babel, and react packages to latest versions; README instructions

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 module.exports = {
   entry: './index.js',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   output: {
     filename: 'bundle.js',
     publicPath: ''
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }]
       }
     ]
   }
